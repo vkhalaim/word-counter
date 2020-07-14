0a9bce7b4ce820834aa031329f3013529f4dab6c
vkhalaim <vasyl.khalaim@ukr.net>
2020-07-14 17:32:58 +0300

    Add models and manager.

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'adfdvf_ewrer-234-%$-sdf32'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
